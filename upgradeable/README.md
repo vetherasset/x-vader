5c032e8b70d4010d1c2e26e3d5f7b4c834105116
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-11-30 22:56:26 -0500

    install hre

--- upgradeable/README.md
+++ upgradeable/README.md
@@ -12,4 +12,6 @@ npx hardhat test
 npx hardhat node
 node scripts/sample-script.js
 npx hardhat help
+
+npx hardhat verify --network mainnet DEPLOYED_CONTRACT_ADDRESS "Constructor argument 1"
 ```
--- upgradeable/hardhat.config.js
+++ upgradeable/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomiclabs/hardhat-ethers")
 require("@openzeppelin/hardhat-upgrades")
+require("@nomiclabs/hardhat-etherscan")
+
+const { alchemyApiKey, mnemonic, etherscanApiKey } = require("./secrets.json")
 
 module.exports = {
-  solidity: "0.8.9",
+    solidity: "0.8.9",
+    networks: {
+        kovan: {
+            url: `https://eth-kovan.alchemyapi.io/v2/${alchemyApiKey}`,
+            accounts: { mnemonic: mnemonic },
+        },
+    },
+    etherscan: {
+        apiKey: etherscanApiKey,
+    },
 }
