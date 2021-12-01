faed6e2d44ef8c3dce7bf33855a6d21c68be5e81
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-12-01 02:40:07 -0500

    readme

--- upgradeable/README.md
+++ upgradeable/README.md
@@ -32,3 +32,10 @@ npx hardhat verify --network mainnet DEPLOYED_CONTRACT_ADDRESS "Constructor argu
 # upgrade
 npx hardhat run --network kovan scripts/test/upgrade.js
 ```
+
+### Mainnet
+
+### Kovan
+
+-   ProxyAdmin: [0x800eD5623AfaE464aabC957cAb9736D3817060a7](https://kovan.etherscan.io/address/0x800eD5623AfaE464aabC957cAb9736D3817060a7)
+-   XVader (Proxy): [0xfD224C48F57deaCd0D8a5C3C4917eecE472Bb134](https://kovan.etherscan.io/address/0xfD224C48F57deaCd0D8a5C3C4917eecE472Bb134)
--- upgradeable/scripts/constants.js
+++ upgradeable/scripts/constants.js
@@ -1,16 +1,18 @@
 const VADER = {
     mainnet: "0x2602278EE1882889B946eb11DC0E810075650983",
-    // kovan: "0xcCb3AeF7Baa506e2D05193e38e88459F68AC1a8F",
+    kovan: "0xcCb3AeF7Baa506e2D05193e38e88459F68AC1a8F",
     // test token
-    kovan: "0x1fd03e4eA209497910fACE52e5ca39124ef2E8BE",
+    // kovan: "0x1fd03e4eA209497910fACE52e5ca39124ef2E8BE",
 }
 
+// address of proxy
 const XVADER = {
     mainnet: "",
-    // kovan: "",
-    // proxy admin 0x24B24A311d2Df62f1DFbf7cba73aB0A4173D85dA
+    kovan: "0xfD224C48F57deaCd0D8a5C3C4917eecE472Bb134",
+
     // x vader with test token
-    kovan: "0x0AA1056Ee563C14484fCC530625cA74575C97512",
+    // proxy admin 0x24B24A311d2Df62f1DFbf7cba73aB0A4173D85dA
+    // kovan: "0x0AA1056Ee563C14484fCC530625cA74575C97512",
 }
 
 module.exports = {
