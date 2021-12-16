fe5164c47a116734e7cfc135de435f1b11277d29
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-12-16 02:08:10 -0500

    start cooldown

--- contracts/XVaderV2.sol
+++ contracts/XVaderV2.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-import "../Ownable.sol";
+import "./Ownable.sol";
 
 contract XVaderV2 is
     Initializable,
@@ -13,15 +13,16 @@ contract XVaderV2 is
     ReentrancyGuardUpgradeable,
     Ownable
 {
+    event SetStakeDuration(uint duration);
+
     /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
     IERC20 public immutable vader;
 
-    // used for testing upgrade
-    uint public version;
+    // Minimum time staked before unstake can be called
+    uint public stakeDuration;
 
-    function test() external onlyOwner {
-        version += 1;
-    }
+    // Mapping from staker to timestamp of last stake
+    mapping(address => uint) public stakedAt;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(IERC20 _vader) initializer {
@@ -29,8 +30,17 @@ contract XVaderV2 is
         vader = _vader;
     }
 
+    function setStakeDuration(uint _stakeDuration) external onlyOwner {
+        require(_stakeDuration > 0, "duration = 0");
+        stakeDuration = _stakeDuration;
+        emit SetStakeDuration(_stakeDuration);
+    }
+
     // Locks vader and mints xVader
     function enter(uint _amount) external nonReentrant {
+        // NOTE: timestamp is reset for msg.sender
+        stakedAt[msg.sender] = block.timestamp;
+
         // Gets the amount of vader locked in the contract
         uint totalVader = vader.balanceOf(address(this));
         // Gets the amount of xVader in existence
@@ -53,10 +63,48 @@ contract XVaderV2 is
     // Claim back your VADER
     // Unlocks the staked + gained vader and burns xVader
     function leave(uint _shares) external nonReentrant {
+        require(block.timestamp >= stakedAt[msg.sender] + stakeDuration, "time < min");
+
         // Calculates the amount of vader the xVader is worth
         uint vaderAmount = (_shares * vader.balanceOf(address(this))) / totalSupply();
 
         _burn(msg.sender, _shares);
         vader.transfer(msg.sender, vaderAmount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint amount
+    ) internal override {
+        // skip if amount = 0, mint, burn or transfer to self
+        if (amount == 0 || from == address(0) || to == address(0) || from == to) {
+            return;
+        }
+
+        uint fromTimestamp = stakedAt[from];
+        // timestamp may be 0 from V1 implementation
+        if (fromTimestamp > 0) {
+            /*
+            f = stakedAt[from]
+            t = stakedAt[to]
+            a = amount
+            b = balance of to
+
+            if f > t
+                stakedAt[to] = (a * f + b + t) / (a + b)
+            else
+                stakedAt[to] = t
+            */
+            uint toTimestamp = stakedAt[to];
+            if (fromTimestamp > toTimestamp) {
+                uint toBal = balanceOf(to);
+                stakedAt[to] =
+                    (amount * fromTimestamp + toBal * toTimestamp) /
+                    (amount + toBal);
+            }
+        } else {
+            stakedAt[to] = block.timestamp;
+        }
     }
 }
